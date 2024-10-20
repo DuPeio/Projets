b6e881743331910bcdbffdc93b61ad63c29d5617
Pierre MAGIEU <magieu.pierre@gmail.com>
2024-10-20 20:35:31 +0200

    lets go j'ai fait le titre

--- Projets Personnels/Projet Site Projets/script.js
+++ Projets Personnels/Projet Site Projets/script.js
@@ -15,3 +15,29 @@ titrePers.addEventListener("click", ()=>{
         elmt.classList.toggle("pers");
     }
 });
+
+
+// Gérer la taille de la police du titre en fonction du scroll
+const title = document.querySelector('H1'); 
+
+document.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY; 
+    const tailleFont = Math.max(8 - scrollPosition / 250, 5); 
+
+    title.style.fontSize = `${tailleFont}vw`; 
+});
+
+
+// Gérer la position du rond marron
+const rond1 = document.querySelector('.rond1'); 
+const rond2 = document.querySelector('.rond2'); 
+
+document.addEventListener('mousemove', (e) => {
+
+    let x = e.clientX *100 /window.innerWidth+"%";
+    let y = e.clientY *100 /window.innerHeight+"%";
+    rond2.style.left = x;
+    rond2.style.top = y; 
+    rond2.style.transform = "translate(-"+x+",-"+y+")";
+    
+});
